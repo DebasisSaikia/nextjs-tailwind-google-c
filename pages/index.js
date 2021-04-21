5c949a99cdb08a36cf276a54b213fc4b45ccf492
Debasis Saikia <debasishsaikia136@gmail.com>
2021-04-21 23:04:17 +0530

    icons-added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Avatar from '../components/Avatar';
-import { ViewGridIcon, MicrophoneIcon } from '@heroicons/react'
+// import { ViewGridIcon, MicrophoneIcon } from '@heroicons/react'
+import { ViewGridIcon, MicrophoneIcon, SearchIcon } from '@heroicons/react/solid'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -18,11 +20,19 @@ export default function Home() {
         <div className="flex space-x-4 items-center">
           <p className="link">Gmail</p>
           <p className="link">Images</p>
-          <ViewGridIcon />
+          <ViewGridIcon className='h-10 w-10 p-2 cursor-pointer rounded-full hover:bg-gray-100' />
           <Avatar url="https://image.freepik.com/free-vector/cute-cat-gaming-cartoon_138676-2969.jpg" />
         </div>
       </header>
       {/* body element */}
+      <form action="#">
+        <Image src='https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-google-sva-scholarship-20.png' height={100} width={300} />
+        <div>
+          <SearchIcon className='w-5 h-5 mr-3 text-gray-500' />
+          <input type="text" className='focus:outline-none bg-red-500 ' />
+          <MicrophoneIcon className='w-5 h-5 mr-3 text-gray-500' />
+        </div>
+      </form>
       {/* footer elements */}
     </div>
   )
